d3f05f7c847fe405cc96119afef9d569936c3793
Ignacio López Ezcurra <ignanmrk@gmail.com>
2023-10-04 20:13:07 -0300

    se creo projecto con mensaje sout

---
+++ CIEj1/src/ciej1/CIEj1.java
@@ -0,0 +1,14 @@
+
+package ciej1;
+
+
+public class CIEj1 {
+
+
+    public static void main(String[] args) {
+        
+        System.out.println("A commit has been made.");
+        
+    }
+    
+}
